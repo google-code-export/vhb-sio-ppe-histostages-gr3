7329e6b80a13e2f784afe536c79ddd1cf3bd1121
barre.corentin@gmail.com <barre.corentin@gmail.com@24774f4b-8c24-b2ed-740f-964c109870b5>
2012-10-16 15:30:46 +0000

    depot V1.1

---
+++ vhb-sio-ppe-histoStages-gr3/Appli-HistoStages-V1.0/activitesP4.php
@@ -0,0 +1,60 @@
+    <div id="titre">
+      <h1>Activités professionnelles du processus P4 - gras italique pour les activités communes</h1>
+      <table>
+        <thead>
+          <tr>
+             <th class="nomenclature">Nomenclature</th><th class="libelle">Libell&eacute;</th>
+          </tr>
+        </thead>
+        <tr>
+            <td colspan="2" class="domaine">Conception et réalisation d'une solution applicative
+            </td>
+        </tr>
+        <tr>
+             <td>A4.1.1</td><td>Proposition d'une solution applicative</td>
+        </tr>
+        <tr class="commun">
+             <td>A4.1.2</td><td>Conception ou adaptation de l'interface utilisateur d'une solution applicative</td>
+        </tr>
+        <tr class="commun">
+              <td>A4.1.3</td><td>Conception ou adaptation d'une base de donn&eacute;es</td>
+        </tr>
+        <tr>
+              <td>A4.1.4</td><td>D&eacute;finition des caract&eacute;ristiques d'une solution applicative</td>
+        </tr>
+        <tr>
+              <td>A4.1.5</td><td>Prototypage de composants logiciels</td>
+        </tr>
+        <tr>
+              <td>A4.1.6</td><td>Gestion d'environnements de d&eacute;veloppement et de test</td>
+        </tr>
+        <tr class="commun">
+              <td>A4.1.7</td><td>D&eacute;veloppement, utilisation ou adaptation de composants logiciels</td>
+        </tr>
+        <tr class="commun">
+              <td>A4.1.8</td><td>R&eacute;alisation des tests n&eacute;cessaires &agrave; la validation d'&eacute;l&eacute;ments adapt&eacute;s ou d&eacute;velopp&eacute;s </td>
+        </tr>
+        <tr>
+              <td>A4.1.9</td><td>R&eacute;daction d'une documentation technique </td>
+        </tr>
+        <tr>
+            <td>A4.1.10</td><td> R&eacute;daction d'une documentation d'utilisation </td>
+        </tr>
+        <tr>
+            <td colspan="2" class="domaine">Maintenance d'une solution applicative
+            </td>
+        </tr>
+        <tr>
+            <td>A4.2.1</td><td>Analyse et correction d'un dysfonctionnement, d'un probl&egrave;me de qualit&eacute; de service ou de s&eacute;curit&eacute;</td>
+        </tr>
+        <tr>
+            <td>A4.2.2</td><td>Adaptation d'une solution applicative aux &eacute;volutions de ses composants</td>
+        </tr>
+        <tr>
+            <td>A4.2.3</td><td>R&eacute;alisation des tests n&eacute;cessaires &agrave; la mise en production d'&eacute;l&eacute;ments mis &agrave; jour</td>
+        </tr>
+        <tr>
+            <td>A4.2.4</td><td>Mise &agrave; jour d'une documentation technique</td>
+        </tr>
+    </table>
+    </div>
